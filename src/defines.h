39d6bb2b2c86e16af59fb079170a1acccece05ab
wafflekirima <i.ty.wafflegirl@gmail.com>
2026-01-20 18:50:47 +0700

    update for ESP32S3 super mini

--- src/defines.h
+++ src/defines.h
@@ -27,10 +27,10 @@
 
 // Set parameters of IMU and board used
 #ifndef IMU
-#define IMU IMU_AUTO
+#define IMU IMU_ICM45686
 #endif
 #ifndef SECOND_IMU
-#define SECOND_IMU IMU_AUTO
+#define SECOND_IMU IMU_ICM45686
 #endif
 #ifndef BOARD
 #define BOARD BOARD_SLIMEVR_V1_2
@@ -64,15 +64,33 @@
 
 // --- OVERRIDES FOR DEFAULT PINS
 
-// #define PIN_IMU_SDA 14
-// #define PIN_IMU_SCL 12
-// #define PIN_IMU_INT 16
-// #define PIN_IMU_INT_2 13
+#define PIN_IMU_SDA 10
+#define PIN_IMU_SCL 11
+#define PIN_IMU_INT 4
+#define PIN_IMU_INT_2 2
 // #define PIN_BATTERY_LEVEL 17
-// #define LED_PIN 2
+#define LED_PIN 48
 // #define LED_INVERTED true
 // #define BATTERY_SHIELD_RESISTANCE 0
 // #define BATTERY_SHIELD_R1 10
 // #define BATTERY_SHIELD_R2 40.2
-
+#define SENSOR_DESC_LIST                        \
+    SENSOR_DESC_ENTRY(                         \
+        IMU,                                   \
+        PRIMARY_IMU_ADDRESS_ONE,               \
+        IMU_ROTATION,                          \
+        DIRECT_WIRE(PIN_IMU_SCL, PIN_IMU_SDA), \
+        PRIMARY_IMU_OPTIONAL,                  \
+        DIRECT_PIN(PIN_IMU_INT),               \
+        0                                      \
+    )                                          \
+    SENSOR_DESC_ENTRY(                         \
+        SECOND_IMU,                            \
+        SECONDARY_IMU_ADDRESS_TWO,             \
+        SECOND_IMU_ROTATION,                   \
+        DIRECT_WIRE(PIN_IMU_SCL, PIN_IMU_SDA), \
+        SECONDARY_IMU_OPTIONAL,                \
+        DIRECT_PIN(PIN_IMU_INT_2),             \
+        0                                      \
+    )
 // ------------------------------
